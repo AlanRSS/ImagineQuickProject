513de9bb2ca72df8611a6c7aa61266213f488092
Alan Santos <alanrss_1@hotmail.com>
2016-03-30 08:29:44 +0100

    Configuring Startup.cs

--- Hackathon/Startup.cs
+++ Hackathon/Startup.cs
@@ -15,17 +15,19 @@ namespace Hackathon
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.UseIISPlatformHandler();
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            /*app.UseMvc(config => {
+                name: "Default",
+                template: "{controller}/{actoin}/{id?}",
+                defaults: new { controller = "Hack", action = "Inicial"}
+            });*/
         }
 
         // Entry point for the application.
